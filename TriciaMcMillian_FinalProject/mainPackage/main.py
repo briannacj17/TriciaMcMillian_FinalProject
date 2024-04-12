4749fa9461a3fd0b0aa71c03c66164a1c283f0ca
brian <brian@Bri>
2024-04-12 11:46:11 -0400

    update

---
+++ TriciaMcMillian_FinalProject/mainPackage/main.py
@@ -0,0 +1,10 @@
+#main.py
+# Name: Brianna Jarrell, Leonie Troeger, Thomas Gilligan
+# email: jarrelbc@mail.uc.edu, troegele@mail.uc.edu, gilligtp@mail.uc.edu
+# Assignment Number: Final Project
+# Due Date: 4-23-24
+# Course/Section:IS 4010-002
+# Semester/Year: Spring 2024
+# Brief Description of the assignment:
+# Citations:
+# Anything else that's relevant:
